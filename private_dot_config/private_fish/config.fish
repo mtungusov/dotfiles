70f7237f4fdc225820e329482e73d35ce1b2af3e
MTungusov <mtungusov@gmail.com>
2025-02-25 21:17:53 +0300

    fish: OrbStack

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -119,3 +119,7 @@ end
 if type -q /Users/misha/.lmstudio/bin
     set -gx PATH $PATH /Users/misha/.lmstudio/bin
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
