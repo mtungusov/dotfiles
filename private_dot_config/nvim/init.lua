20114c1c079e91bbc3313d2091adefcc036b7cbe
Mikhail Tungusov <mtungusov@gmail.com>
2026-06-30 16:26:28 +0300

    c neovim, add plugins

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,6 +1,12 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- show line num
+vim.o.number = true
+vim.o.cursorline = true
+vim.o.wrap = false
+vim.o.incsearch = true
+
 -- remap leader key
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
@@ -47,4 +53,59 @@ else
     keymap("n", "<C-n>", ":botright vnew<CR>", opts)
     keymap("n", "<Tab>", ":wincmd w<CR>", opts)
     keymap("n", "<S-Tab>", ":wincmd r<CR>", opts)
+    --
+    -- Plugins
+    --
+    -- Bootstrap lazy.nvim
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        if vim.v.shell_error ~= 0 then
+            vim.api.nvim_echo({
+                      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+                      { out, "WarningMsg" },
+                      { "\nPress any key to exit..." },
+                }, true, {})
+                vim.fn.getchar()
+                os.exit(1)
+            end
+        end
+    vim.opt.rtp:prepend(lazypath)
+    -- Setup lazy.nvim
+    require("lazy").setup({
+        {
+            "nvim-telescope/telescope.nvim", version = "*",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-telescope/telescope-file-browser.nvim",
+            },
+            config = function()
+                require("telescope").setup {
+                    extensions = {
+                        file_browser = {
+                            -- disables netrw and use telescope-file-browser in its place
+                            hijack_netrw = true,
+                        },
+                    },
+                }
+                require("telescope").load_extension("file_browser")
+
+                local builtin = require("telescope.builtin")
+                keymap("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+                keymap("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+                keymap("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+            end
+        },
+        {
+            "tpope/vim-fugitive"
+        },
+        {
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            config = function()
+                require('lualine').setup({})
+            end
+        }
+    })
 end
