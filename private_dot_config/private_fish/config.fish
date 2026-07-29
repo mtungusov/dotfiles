eebecc01e16f272462388f8454da29e6c7001552
Mikhail Tungusov <mtungusov@gmail.com>
2026-07-29 12:02:30 +0300

    U compact config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,61 +1,14 @@
-set -g fish_greeting
-
-set -gx TERM "xterm-256color"
-set -gx LANG "en_US.UTF-8"
-set -gx LC_COLLATE "en_US.UTF-8"
-set -gx LC_TYPE "en_US.UTF-8"
-
-# ARCHFLAGS = "-arch arm64" or "-arch x86_64"
-set -gx ARCHFLAGS "-arch $(uname -m)"
-
 # Homebrew
 if type -q /opt/homebrew/bin/brew
-    fish_add_path -g /opt/homebrew/bin
-end
-if type -q /usr/local/bin/brew
-    fish_add_path -g /usr/local/bin
+    eval "$(/opt/homebrew/bin/brew shellenv fish)"
 end
 
-if type -q brew
-    set -l BREW_PREFIX "$(brew --prefix)"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/zlib/lib/pkgconfig"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/ncurses/lib/pkgconfig"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/readline/lib/pkgconfig"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/sqlite/lib/pkgconfig"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/openssl/lib/pkgconfig"
-    set -gxa PKG_CONFIG_PATH "$BREW_PREFIX/opt/tcl-tk/lib/pkgconfig"
-    set -gxa LDFLAGS "-L$(brew --prefix bzip2)/lib"
-    set -gxa LDFLAGS "-L$(brew --prefix sqlite)/lib"
-    set -gxa CPPFLAGS "-I$(brew --prefix bzip2)/include"
-    set -gxa CPPFLAGS "-I$(brew --prefix sqlite)/include"
-end
+set -g fish_greeting
 
-if type -q pkg-config
-    set -gxa LDFLAGS "$(pkg-config zlib --libs)"
-    set -gxa LDFLAGS "$(pkg-config tk --libs)"
-    set -gxa LDFLAGS "$(pkg-config ncurses --libs)"
-    set -gxa LDFLAGS "$(pkg-config readline --libs)"
-    set -gxa LDFLAGS "$(pkg-config openssl --libs)"
-
-    set -gxa CPPFLAGS "$(pkg-config zlib --cflags)"
-    set -gxa CPPFLAGS "$(pkg-config tk --cflags)"
-    set -gxa CPPFLAGS "$(pkg-config readline --cflags)"
-    set -gxa CPPFLAGS "$(pkg-config openssl --cflags)"
-end
-
-# TCL and TK for Python
-# fish_add_path "$BREW_PREFIX/opt/tcl-tk/bin"
-# PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='$(pkg-config tk --cflags)' --with-tcltk-libs='$(pkg-config tk --libs)'"
-
-# Ruby enable YJIT
-# set -gx RUBY_CONFIGURE_OPTS --enable-yjit
+set -gx ARCHFLAGS "-arch $(uname -m)"
 
 # Editor
 set -gx EDITOR "nvim"
-
-if type -q emacs
-   alias e "emacs -Q -nw -l '~/.config/emacs/nano.el'"
-end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -80,36 +33,25 @@ if status is-interactive
     if type -q zoxide
        zoxide init fish | source
     end
-end
 
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-       builtin cd -- "$cwd"
+    if type -q git
+        alias gst   "git status"
+
+        # `git log` with patches shown with difftastic.
+        alias glg "git log -p --ext-diff"
+
+        # Show the most recent commit with difftastic.
+        alias gsh "git show --ext-diff"
     end
-    rm -f -- "$tmp"
 end
 
-# alias go-vpn "ssh -t vpn-work 'tmux -CC new -A -s main'"
-
-if type -q git
-    alias gst   "git status"
-
-    # `git log` with patches shown with difftastic.
-    alias glg "git log -p --ext-diff"
-
-    # Show the most recent commit with difftastic.
-    alias gsh "git show --ext-diff"
+if type -q mise
+    if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
+    end
 end
-
-# if type -q mise
-#     if status is-interactive
-#         mise activate fish | source
-#     else
-#         mise activate fish --shims | source
-#     end
-# end
 
 if type -q direnv
     direnv hook fish | source
@@ -119,11 +61,6 @@ if type -q starship
     starship init fish | source
 end
 
-# Added by LM Studio CLI (lms)
-if type -q /Users/misha/.lmstudio/bin
-    set -gx PATH $PATH /Users/misha/.lmstudio/bin
-end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+# Disable OpenSpec telemetry
+set -gx OPENSPEC_TELEMETRY 0
+set -gx DO_NOT_TRACK 1
