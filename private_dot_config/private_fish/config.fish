071a2c64203753167f62b5f1bd9cf445a3cefd64
Mikhail Tungusov <mtungusov@gmail.com>
2026-08-21 12:19:28 +0300

    Update config.fish
    golangci-lint

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -57,6 +57,10 @@ if type -q direnv
     direnv hook fish | source
 end
 
+if type -q golangci-lint
+    golangci-lint completion fish | source
+end
+
 if type -q starship
     starship init fish | source
 end
